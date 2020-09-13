68f8a69a0205a7abf357dbb733fd01a7de12c74b
Charlie Chengrui Zheng <zhengchengrui@hotmail.com>
2020-09-13 19:32:14 -0400

    Add files via upload

--- spam.py
+++ spam.py
@@ -9,111 +9,157 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 
-#This class simulates the emails
 class email():
+    """This class simulates the emails"""
+    def __init__(self, id, file, score=0):
+        self.id = id  # id of each email
+        text = open(file,'rt').read()  # read the text of the file
+        words = word_tokenize(text) # split the text into words and strip all punctuation
 
-    def __init__(self, id, fileName, score=0):
-        #this is the id of the email
-        self.id = id
-        #read the file
-        f = open(fileName,'rt')
-        #make all words lowercase
-        text = f.read()
-        #split the text into words and strip all punctuation
-        words = word_tokenize(text)
-        #strip stop words and number, only remain the stems
+        # strip stop words and number, only remain the stems
         en_stopwords = stopwords.words('english')
         porter = PorterStemmer();
         self.words = [porter.stem(w.lower()) for w in words if w.isalpha() and not w in en_stopwords]
-        #this is the score of its spamicity
-        self.score = score
 
-    def getId(self):
+        self.score = score  # this is the score of the email's spamicity
+
+    def get_id(self):
         return self.id
 
-    def getWords(self):
+    def get_words(self):
         return self.words
 
-    def getScore(self):
+    def get_score(self):
         return self.score
 
-    def setScore(self, newScore):
-        self.score = newScore
+    def set_score(self, new_score):
+        self.score = new_score
+
 
 class filter():
+    """This class simulates the filter of the mailbox"""
 
     def __init__(self, threshold):
-        # this is the threshold of the spam score
+        """this is the threshold of the spam score"""
         self.threshold = threshold
-        # the inbox stores all hams
-        self.inbox = []
-        # spams store all spams
-        self.spams = []
-        # the bag of words of spams
-        self.spamBOW = {}
-        # the bag of words of hams
-        self.hamBOW = {}
-    
-    #this function determines the spamicity of each word
-    def wordSpamicity(self, word):
-        #the frequency of the word appeared in spams
-        wordInSpam = 0
-        #the frequency of the word appeared in hams
-        wordInHam = 0
+
+        self.inbox = []  # the inbox stores all hams
+        self.spams = []  # spams store all spams
+        self.spam_BOW = {} # the bag of words of spams
+        self.ham_BOW = {}  # the bag of words of hams
+
+
+    def word_spamicity(self, word):
+        """Determines the spamicity of each word.
+
+        Args:
+            word: the word to be calculated
+
+        Returns:
+            the spamicity of the word
+        """
+        word_spam = 0  # the frequency of the word appeared in spams
+        word_ham = 0 # the frequency of the word appeared in hams
+
         #checking the bags of words to set the frequency
-        if word in self.spamBOW.keys(): wordInSpam = self.spamBOW[word]
-        if word in self.hamBOW.keys(): wordInHam = self.hamBOW[word]
+        if word in self.spam_bow.keys():
+            word_spam = self.spam_bow[word]
+        if word in self.ham_bow.keys():
+            word_ham = self.ham_bow[word]
+
         #calculating the probability of a spam word
-        pws = float(wordInSpam/len(self.spams))
-        pwh = float(wordInHam/len(self.inbox))
-        if (pws + pwh) == 0: psw =0
-        else: psw = float(pws/(pws+pwh))
+        pws = float(word_spam/len(self.spams))
+        pwh = float(word_ham/len(self.inbox))
+        if (pws + pwh) == 0:
+            psw =0
+        else:
+            psw = float(pws/(pws+pwh))
         return psw
-        
-    #this function calcuates the spam score of the email according to its words' spamicity
-    def spamScore(self,mail):
-        score = 0;
+
+
+    def spam_score(self, mail):
+        """Calcuates the spam score of the email according to its words' spamicity
+
+        Args:
+            mail: the mail to be calculated
+
+        Returns:
+            the spam score of the email according to its words' spamicity
+
+        """
+        score = 0
+
         #getting the spamicity of each word
-        for w in mail.getWords():
-            spamicity = self.wordSpamicity(w)
-            if spamicity > 0.9: score += 3
-            if spamicity > 0.75 and spamicity <= 0.9: score += 2
-            if spamicity > 0.5 and spamicity <= 0.75: score += 1
+        for w in mail.get_words():
+            spamicity = self.word_spamicity(w)
+            if spamicity >= 0.9:
+                score += 3
+            if spamicity >= 0.75 and spamicity < 0.9:
+                score += 2
+            if spamicity >= 0.5 and spamicity < 0.75:
+                score += 1
         return score
-    
-    #this function receives a new email, calculates its spam score, update the bags of words, and sort the email
+
+
     def receive(self, mail):
-        score = self.spamScore(mail)
-        mail.setScore(score)
-        if score > self.threshold: 
+        """Receives a new email, calculates its spam score, update the bags of words, and sort the email
+
+        Args:
+            mail: the new mail to be received
+
+        """
+
+        # calculates the mail's spam score
+        score = self.spam_score(mail)
+        mail.set_score(score)
+
+        # sort the mail and update the bags of words
+        if score > self.threshold:
             self.spams.append(mail)
-            print(mail.getId() + ' is a spam. Its Spam score is ' + str(mail.getScore()))
-            self.addWords(self.spamBOW, mail.getWords())
-        else: 
+            print(mail.get_id() + ' is a spam. Its Spam score is ' + str(mail.get_score()))
+            self.add_words(self.spam_bow, mail.get_words())
+        else:
             self.inbox.append(mail)
-            print(mail.getId() + ' is not a spam. Its Spam score is ' + str(mail.getScore()))
-            self.addWords(self.hamBOW, mail.getWords())
-    
-    #this function trains the filter
-    def train(self, mail, isSpam):
-        if isSpam is True: 
+            print(mail.get_id() + ' is not a spam. Its Spam score is ' + str(mail.get_score()))
+            self.add_words(self.ham_bow, mail.get_words())
+
+
+    def train(self, mail, is_spam):
+        """Trains the filter
+
+        Args:
+            mail: the training mail
+            is_spam: whether mail is spam or not
+        """
+
+        if is_spam:
             self.spams.append(mail)
-            self.addWords(self.spamBOW, mail.getWords())
-        else: 
+            self.add_words(self.spam_bow, mail.get_words())
+        else:
             self.inbox.append(mail)
-            self.addWords(self.hamBOW, mail.getWords())
-    
+            self.add_words(self.ham_bow, mail.get_words())
+
+
     @staticmethod
-    def addWords(bow, words):
-        checked = set()
+    def add_words(bow, words):
+        """Adds words to the bag of words
+
+        Args:
+            bow: the bag of words
+            words: the words to be added
+
+        """
+        checked = set()  # the checked words
+
         for w in words:
-            if w not in checked: 
-                checked.add(w)
-                if w not in bow.keys(): bow.update({w:1})
-                else: 
-                    recur = bow[w] + 1
-                    bow.update({w:recur})
-            
+            checked.add(w)
+            # update bag of words
+            if w not in bow.keys():
+                bow.update({w:1})
+            else:
+                recur = bow[w] + 1
+                bow.update({w:recur})
+
 
 #creating training and testing data
 spam1 = email('spam1_train','spam1.txt')
@@ -123,12 +169,12 @@ ham1  = email('ham1_train','ham1.txt')
 ham2  = email('ham2_train','ham2.txt')
 ham3  = email('ham3_test','ham3.txt')
 #creating the filter and set the threshold to 10
-spamFilter = filter(10)
+spam_filter = filter(10)
 #training
-spamFilter.train(spam1, True)
-spamFilter.train(spam2, True)
-spamFilter.train(ham1, False)
-spamFilter.train(ham2, False)
+spam_filter.train(spam1, True)
+spam_filter.train(spam2, True)
+spam_filter.train(ham1, False)
+spam_filter.train(ham2, False)
 #testing
-spamFilter.receive(spam3)
-spamFilter.receive(ham3)
+spam_filter.receive(spam3)
+spam_filter.receive(ham3)
